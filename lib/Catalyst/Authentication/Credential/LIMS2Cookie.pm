9333736f967ee2a8ea143bea4d9be98a2cc09e33
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2014-01-29 15:14:58 +0000

    Revert "Added Junk-session placeholder to avoid an unitialised variable warning"
    This reverts commit 50e5ff68bdb3e96376baf0f6e335ece1a5178bee.

--- lib/Catalyst/Authentication/Credential/LIMS2Cookie.pm
+++ lib/Catalyst/Authentication/Credential/LIMS2Cookie.pm
@@ -68,7 +68,7 @@ sub authenticate {
      	($sessionid,$auth_user) = split ":", $text;
      	
      	# Check we have the correct session id for authentication
-     	unless ($sessionid eq $c->sessionid ? $c->sessionid : 'Junk-session'){
+     	unless ($sessionid eq $c->sessionid){
      		$c->log->debug( "Session ID in authentication cookie does not match current session" );
      		return;
      	}
